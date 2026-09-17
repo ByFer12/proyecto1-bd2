4082fdd5e1d6144afb5e295b3fb0b77892edbc76
MichaelMarinReyes <michaelmarin201831260@cunoc.edu.gt>
2026-09-17 02:01:00 -0600

    Conexión con nodo 1 funcionando

--- nodes/node2/fix-node.sh
+++ nodes/node2/fix-node.sh
@@ -17,18 +17,18 @@ echo "=== 4. Volviendo a levantar el entorno ==="
 docker compose up -d
 
 echo "=== 5. Esperando a que MySQL esté listo para recibir conexiones ==="
-until docker exec mysql-node2 mysqladmin ping -h"localhost" -uroot -pnodo2 --silent; do
-    echo "Esperando a que MySQL arranque..."
-    sleep 3
-done
+#until docker exec mysql-node2 mysqladmin ping -h"localhost" -uroot -pnodo2 --silent; do
+#    echo "Esperando a que MySQL arranque..."
+#    sleep 3
+#done
 
 echo "=== 6. ¡Proceso finalizado con éxito! Mostrando logs recientes ==="
 docker logs mysql-node2 --tail 15
 
-echo "=== 7. Configurando el usuario de replicación y el canal de recuperación ==="
-docker exec mysql-node2 mysql -u root -pnodo2 -e "
-CREATE USER 'repl'@'%' IDENTIFIED BY 'nodo2';
-GRANT REPLICATION SLAVE ON *.* TO 'repl'@'%';
-FLUSH PRIVILEGES;
-CHANGE REPLICATION SOURCE TO SOURCE_USER='repl', SOURCE_PASSWORD='nodo2' FOR CHANNEL 'group_replication_recovery';
-"
+#echo "=== 7. Configurando el usuario de replicación y el canal de recuperación ==="
+#docker exec mysql-node2 mysql -u root -pnodo2 -e "
+#CREATE USER 'repl'@'%' IDENTIFIED BY 'nodo2';
+#GRANT REPLICATION SLAVE ON *.* TO 'repl'@'%';
+#FLUSH PRIVILEGES;
+#CHANGE REPLICATION SOURCE TO SOURCE_USER='repl', SOURCE_PASSWORD='nodo2' FOR CHANNEL 'group_replication_recovery';
+#"
